783fbe9425b96f521aabc1167324ad7c1f16b8df
Shohei <Shohei@Shohei-notePC>
2018-11-29 20:47:29 +0900

    a

--- src/GNDE_Processor.pl
+++ src/GNDE_Processor.pl
@@ -17,13 +17,14 @@ require "Shared_Process.pl"; # MyP::
 #my $search_file_path = "../data/human/rev/ID-FT_rev_uniprot-allFlat.txt";
 my $search_file_path = MyName::get_data_file_path($MyName::GNDE_KEY);
 
-my $CODE_ID2RecName = "ID2RecName";
+my $CODE_id_to_full_RecName = "id_to_full_RecName";
 my $CODE_get_all_ID = "get_all_ID";
+my $CODE_name_search = "name_search";
 
 ###
 
 my $id = &get_all_ID();
-my $aaa = &ID2RecName();
+my $aaa = &id_to_full_RecName();
 my $num = @$id;
 for (my $index = 0; $index < $num ; $index++){
 	print("$$id[$index]:$$aaa[$index]\n");
@@ -34,23 +35,25 @@ sub get_all_ID{
 	&_function_framework($CODE_get_all_ID);
 }
 
-sub ID2RecName{
+sub id_to_full_RecName{
 	my $query_id_list_ref= MyP::check_argument_1_ID_list(shift);
 	
 	if($query_id_list_ref eq ""){
-		&_function_framework($CODE_ID2RecName);
+		&_function_framework($CODE_id_to_full_RecName);
 	}else{
-		&_function_framework($CODE_ID2RecName, $query_id_list_ref);
+		&_function_framework($CODE_id_to_full_RecName, $query_id_list_ref);
 	}
 }
 
-sub RecName_serch{
+sub name_search{
+	my ($id_list_ref, $query_name) = &MyP::check_argument_2_ID_list_string(@_);
+	
 	
 }
 
 sub _function_framework{
 	
-	my ($process_code, $query_id_list_ref, $query) = @_;
+	my ($process_code, $query_id_list_ref, $query_name) = @_;
 	
 	my @contents = ();
 	my $search_all = 0;
@@ -85,8 +88,9 @@ sub _function_framework{
 				$pointer++;
 			}
 			
-			if($process_code eq $CODE_ID2RecName){ &_process_ID2RecName($DB, $objPB, \@contents) }
+			if($process_code eq $CODE_id_to_full_RecName){ &_process_id_to_full_RecName($DB, $objPB, \@contents) }
 			if($process_code eq $CODE_get_all_ID){ push(@contents, $this_id) }
+			if($process_code eq $CODE_name_search){ push(@contents, $this_id) }
 		}
 	}
 	
@@ -95,7 +99,7 @@ sub _function_framework{
 	
 }
 
-sub _process_ID2RecName{
+sub _process_id_to_full_RecName{
 	my ( $DB, $objPB, $contents_ref) = @_;
 	
 	while(my $line = <$DB>){
@@ -109,7 +113,7 @@ sub _process_ID2RecName{
 	}
 }
 
-sub _process_RecName_serch{
+sub _process_name_serch{
 	my ( $DB, $objPB, $contents_ref) = @_;
 	
 	while(my $line = <$DB>){
