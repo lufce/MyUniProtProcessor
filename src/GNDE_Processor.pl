9358f489e03d4ca84710a7451921f3e903fb3808
Shohei <Shohei@Shohei-notePC>
2018-12-01 08:43:27 +0900

    t

--- src/GNDE_Processor.pl
+++ src/GNDE_Processor.pl
@@ -23,48 +23,46 @@ my $CODE_name_search = "name_search";
 
 ###
 
-my $id = &get_all_ID();
-my $aaa = &id_to_full_RecName();
-my $num = @$id;
-for (my $index = 0; $index < $num ; $index++){
-	print("$$id[$index]:$$aaa[$index]\n");
-}
+my @ids = ("1433B_HUMAN","SSPO_HUMAN","SSR3_HUMAN");
+my $ans = &id_to_full_RecName(\@ids);
+my @k = keys $$ans[1];
+my @v = values $$ans[1];
+my @a = @{$$ans[0]};
+print("@a\n@v");
 ###
 
 sub get_all_ID{
-	&_function_framework($CODE_get_all_ID);
+	return &_function_framework($CODE_get_all_ID);
 }
 
 sub id_to_full_RecName{
 	my $query_id_list_ref= MyP::check_argument_1_ID_list(shift);
 	
-	if($query_id_list_ref eq ""){
-		&_function_framework($CODE_id_to_full_RecName);
-	}else{
-		&_function_framework($CODE_id_to_full_RecName, $query_id_list_ref);
-	}
+	return &_function_framework($CODE_id_to_full_RecName, $query_id_list_ref);
 }
 
 sub name_search{
 	my ($id_list_ref, $query_name) = &MyP::check_argument_2_ID_list_string(@_);
 	
-	
+	return &_function_framework($CODE_name_search, $id_list_ref, $query_name);
 }
 
 sub _function_framework{
 	
 	my ($process_code, $query_id_list_ref, $query_name) = @_;
 	
-	my @contents = ();
+	my @matched_id = ();
+	my %id_to_code = ();
+	
 	my $search_all = 0;
 	my $pointer = 0;
 	my $id_num = 0;
 
-	if(defined($query_id_list_ref)){
-		$id_num = @$query_id_list_ref;
-	}else{
+	if($query_id_list_ref eq ""){
 		#ID listが無かったから全IDを検索する。
 		$search_all = 1;
+	}else{
+		$id_num = @$query_id_list_ref;
 	}
 	
 	#open database file
@@ -81,48 +79,72 @@ sub _function_framework{
 		if($line =~ m/^ID   (.+?) /){
 			my $this_id = $1;
 			
-			if(!$search_all and !$$query_id_list_ref[$pointer] eq $this_id){
-				#全検索でもなく、調べるべきIDでも無かったら次のIDを探す。
-				next;
-			}else{
-				$pointer++;
+			if($search_all == 0 and $pointer >= $id_num){
+				last;
 			}
 			
-			if($process_code eq $CODE_id_to_full_RecName){ &_process_id_to_full_RecName($DB, $objPB, \@contents) }
-			if($process_code eq $CODE_get_all_ID){ push(@contents, $this_id) }
-			if($process_code eq $CODE_name_search){ push(@contents, $this_id) }
+			if($search_all == 0 and $$query_id_list_ref[$pointer] ne $this_id){
+				#全検索でもなく、調べるべきIDでも無かったら次のIDを探す。
+				next;
+			}
+			
+			$pointer++;
+			
+			if($process_code eq $CODE_id_to_full_RecName){ &_process_id_to_full_RecName($DB, $objPB, $this_id, \%id_to_code) }
+			if($process_code eq $CODE_get_all_ID){ push(@matched_id, $this_id) }
+			if($process_code eq $CODE_name_search){ &_process_name_serch($DB, $objPB, $this_id, \@matched_id, \%id_to_code, $query_name) }
 		}
 	}
 	
 	close $DB;
-	return \@contents;
+	
+	if($process_code eq $CODE_id_to_full_RecName){@matched_id = @$query_id_list_ref;}
+	
+	my @answer_pack = (\@matched_id, \%id_to_code);
+	return \@answer_pack;
 	
 }
 
 sub _process_id_to_full_RecName{
-	my ( $DB, $objPB, $contents_ref) = @_;
+	my ( $DB, $objPB, $this_id, $id_to_code_ref) = @_;
 	
 	while(my $line = <$DB>){
 		$objPB->addNowAndPrint($line);
 		
-		if($line =~ m/DE   RecName: Full=(.+);/){
+		if($line =~ m/^DE   RecName: Full=(.+);/){
 			my $name = $1;
 			$name =~ s/{.+?}//g;
-			push(@$contents_ref, $name);
+			$$id_to_code_ref{$this_id} = $name;
+			last;
 		}
 	}
 }
 
 sub _process_name_serch{
-	my ( $DB, $objPB, $contents_ref) = @_;
+	my ( $DB, $objPB, $this_id, $matched_id_ref, $id_to_code_ref, $regex) = @_;
+	
+	my $code = "";
 	
 	while(my $line = <$DB>){
 		$objPB->addNowAndPrint($line);
 		
-		if($line =~ m/DE   RecName: Full=(.+);/){
+		if($line =~ m|//|){
+			if($code ne ""){
+				$code = substr($code,-2);
+				push(@$matched_id_ref, $this_id);
+				$$id_to_code_ref{$this_id} = $code;
+			}
+			
+			last;
+		}
+		
+		if($line =~ m/=(.+?);/){
 			my $name = $1;
-			$name =~ s/{.+?}//g;
-			push(@$contents_ref, $name);
+			if($name =~ m/$regex/){
+				
+				#Simpleデータコードとしてエンコード。
+				$code = $code.$name."*,";
+			}
 		}
 	}
 }
